338988769c94c756edb758f544fe98a1168d4b59
Didyon-Mondlhane <mondlhanedj5@gmail.com>
2025-05-10 14:21:51 +0200

    chore: configura estrutura inicial de pastas em Spring Boot e Maven

---
+++ src/test/java/com/didyon/comparison_api/ComparisonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didyon.comparison_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparisonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
